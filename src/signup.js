ac498325daed80ac3d5c4e43a74dcb942abb899b
Ajith madhan <ajithmadhan.dev113@gmail.com>
2021-06-01 15:40:22 +0530

    Added signup and fixed other bugs

--- src/signup.js
+++ src/signup.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import pic from '../src/images/pic.png';
-import {Link} from 'react-router-dom'
-
-
+import {Link,useHistory} from 'react-router-dom'
+import { API } from './Backend';
+import axios from 'axios';
 
 
 
@@ -19,8 +19,43 @@ const useStyles = makeStyles((theme) => ({
     },
   
   }));
-  
+
 const Signup = () => {
+  let history=useHistory();
+  const [user, setuser] = useState({
+    name:'',
+    lastname:'',
+    email:'',
+    userinfo:'',
+    password:'',
+    cpassword:''
+  });
+  const {name,lastname,email,userinfo,password,cpassword}=user;
+
+  const handleChange =name => e=>{
+    setuser({...user,[name]:e.target.value})
+  }
+  
+  const signupUser =(e)=>{
+      e.preventDefault();
+      if(name=='' || lastname ==''|| email==''||userinfo==''||password==''||cpassword==''){
+        return alert("please fill all the fields")
+      }
+      let payload = user;
+
+        axios.post(`${API}/signup`, payload,{ 
+          withCredentials: true,
+        })
+        .then(res=>{
+          alert("success")
+          history.push("/signin");
+        })
+        .catch(err =>{
+      
+          console.log(err.response.data);
+        })
+  }
+
     const classes = useStyles();
     return (
         <div className="App">
@@ -29,13 +64,13 @@ const Signup = () => {
                 <div className="form_container">
                 <h3>Sign up</h3>
       <form className={classes.root} noValidate autoComplete="off">
-      <Input required placeholder="Name"  />
-      <Input required placeholder="Last name"  />
-      <Input type="email" required placeholder="Email"  />
-      <Input type="text" required placeholder="User Info"  />
-      <Input type="password" required placeholder="Password"  />
-      <Input type="password" required placeholder="Conform Password"  />
-      <button className="btn_signin">Sign up</button>
+      <Input required placeholder="Name" value={name} onChange={handleChange('name')} />
+      <Input required placeholder="Last name"  value={lastname} onChange={handleChange('lastname')}/>
+      <Input type="email" required placeholder="Email"  value={email} onChange={handleChange('email')}/>
+      <Input type="text" required placeholder="User Info"  value={userinfo} onChange={handleChange('userinfo')}/>
+      <Input type="password" required placeholder="Password"  value={password} onChange={handleChange('password')}/>
+      <Input type="password" required placeholder="Conform Password"  value={cpassword} onChange={handleChange('cpassword')}/>
+      <button className="btn_signin" onClick={signupUser}>Sign up</button>
      
     
      
